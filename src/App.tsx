90ccd404bf957b3b8e1ade39ca398e597aed5df7
Azeez faborode <faborode22@gmail.com>
2024-09-29 05:16:28 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Counter from "./Typescript/Counter"
+import Greeting from "./Typescript/Greeting"
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Greeting name={"azeez"} />
+    <Greeting name={"Tunde"} />
+    <Counter />
+    
+    </>
+  )
+}
+
+export default App
